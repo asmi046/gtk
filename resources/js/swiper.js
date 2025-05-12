c019ee190dc56f0096c1555f29e1717de528ce81
asmi046 <asmi046@gmail.com>
2025-05-13 01:02:05 +0300

    Swiper Component

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,26 +1,63 @@
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+// import Swiper from 'swiper';
+// import { Navigation } from 'swiper/modules';
 
-import 'swiper/scss';
-import 'swiper/scss/navigation';
+// import 'swiper/scss';
+// import 'swiper/scss/navigation';
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation],
-  slidesPerView: 4,
-  speed: 400,
-  spaceBetween: 100,
-  loop: true,
-    breakpoints: {
-        640: {
+// const swiper = new Swiper('.swiper', {
+//     modules: [Navigation],
+//   slidesPerView: 4,
+//   speed: 400,
+//   spaceBetween: 100,
+//   loop: true,
+//     breakpoints: {
+//         640: {
+//             slidesPerView: 2,
+//         },
+//         1024: {
+//             slidesPerView: 4
+//         }
+//     },
+//   navigation: {
+//     nextEl: '#swiper-next',
+//     prevEl: '#swiper-prev',
+//   },
+
+// });
+
+
+import { register } from 'swiper/element/bundle';
+register();
+
+const any_tours = document.getElementById('ptn_swiper');
+
+if (any_tours) {
+    const swiperParams = {
+        slidesPerView: 1.5,
+        spaceBetween: 48,
+        loop: true,
+        autoplay: {
+            delay: 4500,
+        },
+        breakpoints: {
+
+         640: {
             slidesPerView: 2,
         },
         1024: {
             slidesPerView: 4
         }
-    },
-  navigation: {
-    nextEl: '.swiper-next',
-    prevEl: '.swiper-prev',
-  },
+        },
 
-});
+        navigation: {
+            nextEl: '#swiper-next',
+            prevEl: '#swiper-prev',
+        }
+      };
+
+      // now we need to assign all parameters to Swiper element
+      Object.assign(any_tours, swiperParams);
+
+      // and now initialize it
+      any_tours.initialize();
+}
--- resources/views/components/our-clients.blade.php
+++ resources/views/components/our-clients.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h2 class="section-title">Наши клиенты</h2>
         <div class="clients__inner">
-            <div class="swiper">
+            {{-- <div class="swiper" id="ptn_swiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide"><img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
                     </div>
@@ -34,10 +34,41 @@
                             alt="">
                     </div>
                 </div>
-            </div>
+            </div> --}}
+
+            <swiper-container id="ptn_swiper" init="false">
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+                    <swiper-slide>
+                        <img src="{{ asset('img/swiper_icon/egoryevsk_icon.svg') }}" alt="">
+                    </swiper-slide>
+            </swiper-container>
+
             <div class="swiper-nav">
-                <button class="swiper-prev swiper-btn" aria-label="Предыдущий слайд"></button>
-                <button class="swiper-next swiper-btn" aria-label="Следующий слайд"></button>
+                <button id="swiper-prev" class="swiper-prev swiper-btn" aria-label="Предыдущий слайд"></button>
+                <button id="swiper-next" class="swiper-next swiper-btn" aria-label="Следующий слайд"></button>
             </div>
         </div>
     </div>
